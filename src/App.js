b7b75b95707ddfe84f8dc9cb719c287eec906806
Ryuga Kobayashi <kobayashi114364@gmail.com>
2021-03-22 12:46:53 +0900

    leran jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hello world
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+  render() {
+    return(
+      <React.Fragment>
+        <label htmlFor="うまぴょい">うまぴょい</label>
+        <input type="text" onChange={(() => {console.log("うまぴょい")})}></input>
+      </React.Fragment>
+    )
+  }
 }
 
-export default App;
