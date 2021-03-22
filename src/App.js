4380f4df9f46b52b019b1f2db45667817ae84ac3
Ryuga Kobayashi <kobayashi114364@gmail.com>
2021-03-22 16:46:07 +0900

    learn components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 
 
-export default class App extends React.Component {
-  render() {
-    return(
-      <React.Fragment>
-        <label htmlFor="うまぴょい">うまぴょい</label>
-        <input type="text" onChange={(() => {console.log("うまぴょい")})}></input>
-      </React.Fragment>
-    )
-  }
+// export default class App extends React.Component {
+//   render() {
+//     return(
+//       <React.Fragment>
+//         <label htmlFor="うまぴょい">うまぴょい</label>
+//         <input type="text" onChange={(() => {console.log("うまぴょい")})}></input>
+//       </React.Fragment>
+//     )
+//   }
+// }
+const App = () => {
+  return(
+    <div>
+      <Dog/>
+      <Dog/>
+      <Dog/>
+      <Dog/>
+      <Dog/>
+    </div>
+  )
 }
+const Dog = () => {
+  return <div>バウ!!</div>
+}
+
+export default App;
 
